b65162a6dd7732a8daed2563bcddea96e58f3cb6
Anthony Bocci <boccianthony@yahoo.fr>
2019-06-05 08:16:46 +0200

    Create a Reader trait every sensor will implement
    Every sensor will have to implement a Trait, "Reader". This trait
    contains a method used to read the temperature and returns a possible
    ExportedData, or an error.
    
    It is a base to manage every sensor the same way.

---
+++ src/sensors/reader.rs
@@ -0,0 +1,33 @@
+//Copyright 2019 Anthony Bocci
+//This file is part of Temperature.
+// Temperature is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// Temperature is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Temperature.  If not, see <https://www.gnu.org/licenses/>.
+
+/*!
+ * Module reader.
+ *
+ * Here is defined the Reader trait.
+ */
+
+use crate::models::ExportedData;
+use crate::models::errors::ReadError;
+
+/**
+ * A trait used to read a temperature.
+ *
+ * Every sensor has to implement it.
+ */
+pub trait Reader {
+    /// Read a temperature.
+    fn read(&self) -> Result<ExportedData, ReadError>;
+}
