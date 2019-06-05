1de11929fc8c8dda41791dcd5e75b741a1c8fe56
Anthony Bocci <boccianthony@yahoo.fr>
2019-06-05 21:20:56 +0200

    Create a RegularInterval manager
    The RegularInterval is a manger that reads / exports at a regular
    interval, every X seconds.
    It's not working yet, the Manager implementation is empty. It's mainly
    a schema, it will be implemented later.

---
+++ src/managers/regular_interval.rs
@@ -0,0 +1,46 @@
+//Copyright 2019 Anthony Bocci
+//This file is part of Temperature.
+// Temperature is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// Temperature is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Temperature.  If not, see <https://www.gnu.org/licenses/>.
+
+/*!
+ * Module regular_interval.
+ *
+ * Here are defined the RegularInterval structure and its methods.
+ */
+
+use super::Manager;
+
+/**
+ * A manager that runs in a regular manner.
+ *
+ * It's useful when you want to read / export the temperature
+ * every 30 seconds for example.
+ */
+pub struct RegularInterval {
+    /// The time interval between two runs, in seconds.
+    pub interval: u32,
+}
+
+impl Manager for RegularInterval {
+    // Read and export the temperature every `interval` seconds.
+    fn run() {
+        //TODO: Read the configuration
+        //      Instantiate the reader
+        //      Read the temperature
+        //      Instantiate the exporter(s)
+        //      Export the temperature
+        //      Wait `interval` seconds
+        //      Repeat
+    }
+}
